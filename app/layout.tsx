9ebfb8c195f8556cdbcea387e9ead96706237831
Maryna <marina.avramenko123@gmail.com>
2025-03-27 10:46:38 +0200

    add task solution

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 	children: React.ReactNode
 }>) {
 	return (
-		<html lang='en' data-theme='light'>
+		<html lang='en' data-theme='light' suppressHydrationWarning={true}>
 			<body className={`${geistSans.variable} ${geistMono.variable}`}>
 				{children}
 			</body>
